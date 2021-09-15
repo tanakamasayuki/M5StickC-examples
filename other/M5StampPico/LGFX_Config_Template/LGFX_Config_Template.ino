290c6b2cdd96c8eb94e8dea26d16d737e7fb9267
root <akiraneko@gmail.com>
2021-09-16 08:03:12 +0900

    Add LGFX_Config_Template

---
+++ other/M5StampPico/LGFX_Config_Template/LGFX_Config_Template.ino
@@ -0,0 +1,45 @@
+#define LGFX_USE_V1
+#include <LovyanGFX.hpp>
+
+#include "lgfx_user/M5UnitOLED.hpp"
+#include "lgfx_user/M5UnitLCD.hpp"
+#include "LGFX_ESP32_ST7735S_SPI.hpp"
+#include "LGFX_ESP32_ST7789_SPI.hpp"
+
+//M5UnitOLED display(/*SDA*/32, /*SCL*/33);
+//M5UnitLCD display(/*SDA*/32, /*SCL*/33);
+//LGFX_ESP32_ST7735S_SPI/*0.96*/ display(/*width*/80, /*height*/160, /*sclk(SCL)*/25, /*mosi(SDA)*/22, /*rst*/21, /*dc*/19, /*cs*/18,/*bl*/ -1,/*miso*/ -1, /*busy*/ -1);
+//LGFX_ESP32_ST7789_SPI/*1.14*/ display(/*width*/135, /*height*/240, /*sclk(SCL)*/25, /*mosi(SDA)*/22, /*rst*/21, /*dc*/19, /*cs*/18,/*bl*/ -1,/*miso*/ -1, /*busy*/ -1);
+LGFX_ESP32_ST7789_SPI/*1.3*/ display(/*width*/240, /*height*/240, /*sclk(SCL)*/25, /*mosi(SDA)*/22, /*rst*/21, /*dc*/19, /*cs*/ -1,/*bl*/18,/*miso*/ -1, /*busy*/ -1);
+//LGFX_ESP32_ST7789_SPI/*1.54*/ display(/*width*/240, /*height*/240, /*sclk(SCL)*/25, /*mosi(SDA)*/22, /*rst*/21, /*dc*/19, /*cs*/18,/*bl*/ -1,/*miso*/ -1, /*busy*/ -1);
+//LGFX_ESP32_ST7789_SPI/*2.0*/ display(/*width*/240, /*height*/320, /*sclk(SCL)*/21, /*mosi(SDA)*/19, /*rst*/18, /*dc*/25, /*cs*/22,/*bl*/ -1,/*miso*/ -1, /*busy*/ -1);
+
+void setup() {
+  // Init
+  display.init();
+}
+
+uint32_t count = ~0;
+void loop(void) {
+  display.startWrite();
+  display.fillScreen(TFT_WHITE);
+
+  display.setRotation(++count & 7);
+  display.setColorDepth((count & 8) ? 16 : 24);
+
+  display.setTextColor(random(65536));
+  display.drawNumber(display.getRotation(), 16, 0);
+
+  display.setTextColor(0xFF0000U);
+  display.drawString("R", 30, 16);
+  display.setTextColor(0x00FF00U);
+  display.drawString("G", 40, 16);
+  display.setTextColor(0x0000FFU);
+  display.drawString("B", 50, 16);
+
+  display.drawRect(10, 10, 20, 20, random(65536));
+
+  display.endWrite();
+
+  delay(1000);
+}
---
+++ other/M5StampPico/LGFX_Config_Template/LGFX_ESP32_ST7789_SPI.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+class LGFX_ESP32_ST7789_SPI : public lgfx::LGFX_Device
+{
+    lgfx::Panel_ST7789  _panel_instance;
+    lgfx::Bus_SPI       _bus_instance;
+    lgfx::Light_PWM     _light_instance;
+
+  public:
+
+    LGFX_ESP32_ST7789_SPI(int width, int height, int sclk, int mosi, int rst, int dc, int cs, int bl, int miso = -1, int busy = -1)
+    {
+      {
+        auto cfg = _bus_instance.config();
+
+        cfg.spi_host = VSPI_HOST;
+        if (cs == -1) {
+          cfg.spi_mode = 3;
+        } else {
+          cfg.spi_mode = 0;
+        }
+        cfg.freq_write = 40000000;
+        cfg.freq_read  = 15000000;
+        cfg.spi_3wire  = true;
+        cfg.use_lock   = true;
+        cfg.dma_channel = 1;
+        cfg.pin_sclk = sclk;
+        cfg.pin_mosi = mosi;
+        cfg.pin_miso = miso;
+        cfg.pin_dc   = dc;
+
+        _bus_instance.config(cfg);
+        _panel_instance.setBus(&_bus_instance);
+      }
+
+      {
+        auto cfg = _panel_instance.config();
+
+        cfg.pin_cs           = cs;
+        cfg.pin_rst          = rst;
+        cfg.pin_busy         = busy;
+
+        cfg.memory_width     = 240;
+        cfg.memory_height    = 320;
+        cfg.panel_width      = width;
+        cfg.panel_height     = height;
+        if (width == 80 && height == 160) {
+          // 0.96
+          cfg.offset_x       = 52;
+          cfg.offset_y       = 40;
+        } else {
+          cfg.offset_x       = 0;
+          cfg.offset_y       = 0;
+        }
+        cfg.offset_rotation  = 2;
+        cfg.dummy_read_pixel = 16;
+        cfg.dummy_read_bits  = 1;
+        cfg.readable         = true;
+        cfg.invert           = true;
+        cfg.rgb_order        = false;
+        cfg.dlen_16bit       = false;
+        cfg.bus_shared       = true;
+
+        _panel_instance.config(cfg);
+      }
+
+      {
+        auto cfg = _light_instance.config();
+
+        cfg.pin_bl = bl;
+        cfg.invert = false;
+        cfg.freq   = 44100;
+        cfg.pwm_channel = 7;
+
+        _light_instance.config(cfg);
+        _panel_instance.setLight(&_light_instance);
+      }
+
+      setPanel(&_panel_instance);
+    }
+};
